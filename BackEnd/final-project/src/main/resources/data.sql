dc5d50843e7454615584b71ab77faf2ac4cf6c38
Vilgheff <vn29082000@gmail.com>
2023-07-27 19:34:42 +0700

    Update data.sql

--- BackEnd/final-project/src/main/resources/data.sql
+++ BackEnd/final-project/src/main/resources/data.sql
@@ -4,7 +4,7 @@
 
 
 INSERT INTO courses (name, price, desciption, rating, enroll, active, list_of_video, thumbnail)
-    VALUES ('Java core',100, 'Everything about java', 4.5, 200, 1,'https://youtu.be/QDKsQQr-1wA,https://youtu.be/KJqFJ3-lTcA,https://youtu.be/VBviWbusHd8','https://res.cloudinary.com/dzblifjhf/image/upload/v1690124299/e-learning/course2_ulmbyk.png');
+    VALUES ('Java Complete Course',100, 'Everything about java', 4.5, 200, 1,'https://youtu.be/yRpLlJmRo2w,https://youtu.be/LusTv0RlnSU,https://youtu.be/I5srDu75h_M','https://res.cloudinary.com/dzblifjhf/image/upload/v1690124299/e-learning/course2_ulmbyk.png');
 INSERT INTO courses (name, price, desciption, rating, enroll,active, list_of_video, thumbnail)
 VALUES ('Spring boot',100, 'Spring boot course', 4.7, 500, 1,'https://youtu.be/QDKsQQr-1wA,https://youtu.be/KJqFJ3-lTcA,https://youtu.be/VBviWbusHd8','https://res.cloudinary.com/dzblifjhf/image/upload/v1690124298/e-learning/course1_daopha.jpg');
 INSERT INTO courses (name, price, desciption, rating, enroll, active, list_of_video, thumbnail)
